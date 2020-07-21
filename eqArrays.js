81f05f1efa530d599fd7cf03b5c6e5f58d0cd0d9
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-21 22:20:51 +0000

    created eqArrays.js on lotide

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😨😨😨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function (array1,array2){
+  let result = true;
+  for (let i = 0; i < array1.length; i++){
+    if(array1[i] === array2 [i]){
+      if(i === array1.length - 1){
+        result = true;
+      }
+    } else{
+      result = false;
+      break;
+    }
+  }
+  console.log(result);
+}
+
