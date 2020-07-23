677b900bed89c36d6a123d773b9a6906a862a113
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-23 17:26:17 +0000

    modified my assertEqualArrays.js

--- assertEqualArrays.js
+++ assertEqualArrays.js
@@ -1,8 +1,16 @@
-const eqArrays = function (arr1,arr2){
+const eqArrays = function (array1,array2){
   let result = true;
-  for (let i = 0; i < arr1.length; i++){
-    if(arr1[i] === arr2 [i]){
-      if(i === arr1.length - 1){
+  let longerLength = 0;
+
+  if(array1.length > array2.length){
+    longerLength = array1.length;
+  } else{
+    longerLength = array2.length;
+  }
+
+  for (let i = 0; i < longerLength; i++){
+    if(array1[i] === array2 [i]){
+      if(i === array1.length - 1){
         result = true;
       }
     } else{
@@ -10,6 +18,7 @@ const eqArrays = function (arr1,arr2){
       break;
     }
   }
+
   return result;
 }
 
@@ -19,7 +28,23 @@ const assertEqualArrays = function(array1,array2){
   if(assertionResult === true){
     console.log(`✔️✔️✔️ Assertion Passed: ${array1} === ${array2}`);
   } else {
-    console.log(`😨😨😨 Assertion Failed: ${array1} === ${array2}`);
+    console.log(`😨😨😨 Assertion Failed: ${array1} !== ${array2}`);
   }
 }
+
+assertEqualArrays([ 'age:18',
+'age:23',
+'age:12',
+'age:34',
+'age:15',
+'age:90',
+'age:76',
+'age:34' ],[ 'age:18',
+'age:23',
+'age:12',
+'age:34',
+'age:15',
+'age:90',
+'age:76',
+'age:34' ]);
 
