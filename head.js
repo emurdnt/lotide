866451abe993a883f16ff14846ebdbf8589c18b2
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-27 20:13:11 +0000

    used mocha and chai to test head.js

--- head.js
+++ head.js
@@ -1,13 +1,3 @@
-// FUNCTION IMPLEMENTATION
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`😨😨😨 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
