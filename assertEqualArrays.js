db2806371cbdc555cc28f839d71fcc9eccbd578e
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-27 16:32:00 +0000

    refactored asserEqualArrays to require eqArraysand export assertEqualArrays. Created assertEqualArraysTest.js

--- assertEqualArrays.js
+++ assertEqualArrays.js
@@ -1,26 +1,28 @@
-const eqArrays = function (array1,array2){
-  let result = true;
-  let longerLength = 0;
+// const eqArrays = function (array1,array2){
+//   let result = true;
+//   let longerLength = 0;
 
-  if(array1.length > array2.length){
-    longerLength = array1.length;
-  } else{
-    longerLength = array2.length;
-  }
+//   if(array1.length > array2.length){
+//     longerLength = array1.length;
+//   } else{
+//     longerLength = array2.length;
+//   }
 
-  for (let i = 0; i < longerLength; i++){
-    if(array1[i] === array2 [i]){
-      if(i === array1.length - 1){
-        result = true;
-      }
-    } else{
-      result = false;
-      break;
-    }
-  }
+//   for (let i = 0; i < longerLength; i++){
+//     if(array1[i] === array2 [i]){
+//       if(i === array1.length - 1){
+//         result = true;
+//       }
+//     } else{
+//       result = false;
+//       break;
+//     }
+//   }
 
-  return result;
-}
+//   return result;
+// }
+
+const eqArrays = require('./eqArrays');
 
 const assertEqualArrays = function(array1,array2){
   let assertionResult = eqArrays(array1,array2);
@@ -32,19 +34,6 @@ const assertEqualArrays = function(array1,array2){
   }
 }
 
-assertEqualArrays([ 'age:18',
-'age:23',
-'age:12',
-'age:34',
-'age:15',
-'age:90',
-'age:76',
-'age:34' ],[ 'age:18',
-'age:23',
-'age:12',
-'age:34',
-'age:15',
-'age:90',
-'age:76',
-'age:34' ]);
+module.exports = assertEqualArrays;
+
 
---
+++ test/assertEqualArraysTest.js
@@ -0,0 +1,17 @@
+const assertEqualArrays = require('../assertEqualArrays');
+
+assertEqualArrays([ 'age:18',
+'age:23',
+'age:12',
+'age:34',
+'age:15',
+'age:90',
+'age:76',
+'age:34' ],[ 'age:18',
+'age:23',
+'age:12',
+'age:34',
+'age:15',
+'age:90',
+'age:76',
+'age:34' ]);
