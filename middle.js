26b6f2d2e19cb1964e942e1026fe107b239a3c86
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-27 20:13:41 +0000

    used mocha and chai to test middle.js

--- middle.js
+++ middle.js
@@ -32,5 +32,5 @@ const middle = function (arr){
 // console.log(middle([1])) ;// => [2, 3]
 // console.log(middle([1, 2]));
 // console.log(middle([1, 2, 3])); // => [2]
-// console.log(middle([1, 2, 3, 4, 5,6]))// => [3]
+console.log(middle([1, 2, 3, 4, 5,6]))// => [3]
 module.exports = middle;
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,19 @@
 // const eqArrays = require('../eqArrays');
-const assertEqualArrays = require('../assertEqualArrays');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertEqualArrays(middle([1, 2, 3]),[1,2,3]);
+// assertEqualArrays(middle([1, 2, 3]),[1,2,3]);
 
-assertEqualArrays(middle([1, 2, 3,4]),[2,3]);
+// assertEqualArrays(middle([1, 2, 3,4]),[2,3]);
+
+describe("#middle", () => {
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5,6]), [ 3, 4 ]);
+  });
+  it("returns '3' [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]),[3]); 
+  });
+  it("returns [] for [] ", () => {
+    assert.deepEqual(middle([]), []); 
+  });
+});
